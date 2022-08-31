bc14acdcf68c2a00c5ed935b712ca3d38c955de0
ByoungChul <skyksit@gmail.com>
2022-08-31 14:26:31 +0900

    fixed script

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
-Events.on(WorldLoadEvent, event => {
-  var text = Vars.state.getSector()
-  Vars.ui.hudfrag.showToast(text)
-}
+Events.on(ClientLoadEvent, (event) => {
+  var text = Vars.state.getSector();
+  Vars.ui.hudfrag.showToast(text);
+
+  const myDialog = new BaseDialog("Dialog Title");
+  // Add "go back" button
+  myDialog.addCloseButton();
+  // Add text to the main content
+  myDialog.cont.add("Goodbye.");
+  // Show dialog
+  myDialog.show();
+});
