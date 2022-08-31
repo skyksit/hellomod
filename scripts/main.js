5b634899df3746d2d378aa40ec9d7fffc1d96c8a
ByoungChul <skyksit@gmail.com>
2022-08-31 15:30:19 +0900

    fixed scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 Events.on(ClientLoadEvent, (event) => {
+  var startid = Planets.serpulo.startSector;
   const myDialog = new BaseDialog("Dialog Title");
   // Add "go back" button
   myDialog.addCloseButton();
   // Add text to the main content
-  myDialog.cont.add(Planets.serpulo.getLastSector().id);
+  myDialog.cont.add(startid);
   // Show dialog
   myDialog.show();
 });
