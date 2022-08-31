bbd85028f208eb938adb855f10331cb2d896e606
ByoungChul <skyksit@gmail.com>
2022-08-31 15:06:45 +0900

    fixed script

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,36 @@
 Events.on(ClientLoadEvent, (event) => {
-  var sectors = Vars.state.getSector();
-  Vars.ui.hudfrag.showToast(sectors);
+  var sector198 = Planets.serpulo.getSector(198);
 
   const myDialog = new BaseDialog("Dialog Title");
   // Add "go back" button
   myDialog.addCloseButton();
   // Add text to the main content
-  myDialog.cont.add(sectors);
+  myDialog.cont.add(sector198.name());
   // Show dialog
   myDialog.show();
 });
+
+/**
+ //serpulo presets
+const goldMountains = new SectorPreset("goldMountains", Planets.serpulo, 172);
+goldMountains.captureWave = 30;
+goldMountains.difficulty = 4;
+
+const goldMines = new SectorPreset("goldMines", Planets.serpulo, 205);
+goldMines.difficulty = 7;
+
+const abandonedDesert = new SectorPreset("abandonedDesert", Planets.serpulo, 157);
+abandonedDesert.captureWave = 30;
+abandonedDesert.difficulty = 8;
+
+const reignLair = new SectorPreset("eradLair", Planets.serpulo, 122);
+reignLair.captureWave = 30;
+reignLair.difficulty = 10;
+
+module.exports = {
+  goldMountains: goldMountains,
+  goldMines: goldMines,
+  abandonedDesert: abandonedDesert,
+  reignLair: reignLair
+};
+ */
