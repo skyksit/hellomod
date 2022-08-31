04f354ee8b7ceeedad0785b4349d01ef209157b0
Richard(Kim,ByoungChul) <skyksit@gmail.com>
2022-09-01 08:24:25 +0900

    f

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
-Events.on(WorldLoadEvent, (e) => {
+Events.on(ClientLoadEvent, () => {
   var winWave = Vars.state.rules.winWave;
-  var hasSector = Vars.state.hasSector();
-  var startSector = Planets.serpulo.startSector;
   var sectors = Planets.serpulo.sectors;
 
   var s223 = sectors.items[223];
@@ -10,13 +8,38 @@ Events.on(WorldLoadEvent, (e) => {
   // Add "go back" button
   myDialog.addCloseButton();
   // Add text to the main content
-  myDialog.cont.add("sectors[223].info.wave = " + s223.info.wave);
-  myDialog.cont.add("winWave = " + Vars.state.rules.winWave);
-  myDialog.cont.add("sectors.size = " + sectors.size);
+  myDialog.cont.add("sectors[223].info.wave = " + s223.info.wave + "\n");
+  myDialog.cont.add(
+    "Vars.state.rules.winWave = " + Vars.state.rules.winWave + "\n"
+  );
   // Show dialog
   myDialog.show();
+  //Toast message example
+  // Vars.ui.hudfrag.showToast("Hello World!");
 });
 
+Events.on(ClientLoadEvent, () => {
+  let uiPlanet = Vars.ui.planet;
+  uiPlanet.shown(() => {
+    if (uiPlanet.mode === PlanetDialog.Mode.look) {
+      uiPlanet.fill(
+        cons((t) => {
+          t.top().right();
+          t.defaults().size(200, 54);
+          t.button("Redirect", Icon.upOpen, () => {
+            let sectors = Planets.serpulo.sectors;
+            let groundZero = sectors.get(15);
+            uiPlanet.showSelect(groundZero, (d) => {
+              sectors.each((e) => {
+                e.info.destination = d;
+              });
+            });
+          });
+        })
+      );
+    }
+  });
+});
 /**
  //serpulo presets
 const goldMountains = new SectorPreset("goldMountains", Planets.serpulo, 172);
